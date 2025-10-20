6b385d5b4b02e8c933f8b2a192fb495b41d4b654
Andrei-Alin Socoteala <Andrei-Alin.Socoteala@endava.com>
2025-10-20 11:18:28 +0300

    Chapter 1

---
+++ Chapter 1/KitchenSink.java
@@ -0,0 +1,16 @@
+public class KitchenSink {
+    private int numForks;
+
+    public static void main(String[] args) {
+        int numKnives;
+        System.out.print("""
+          "# forks = " + numForks +
+           " # knives = " + numKnives +
+          # cups = 0""");
+        var blocky = """
+           squirrel \s
+           pigeon   \
+           termite""";
+        System.out.print(blocky);
+    }
+}
---
+++ Chapter 1/River.java
@@ -0,0 +1,16 @@
+public class River {
+    int Depth = 1;
+    float temp = 50.0f;
+    public void flow() {
+        for (int i = 0; i < 1; i++) {
+            int depth = 2;
+            depth++;
+            temp--;
+        }
+        System.out.println(depth);
+        System.out.println(temp);
+    }
+    public static void main(String... s) {
+        new River().flow();
+    }
+}
